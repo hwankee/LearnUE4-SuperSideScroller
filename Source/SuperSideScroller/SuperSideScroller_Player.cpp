96c69f98ebcfeb2fed0ebc8d71c86a696bf853d9
hwankee <abcy100@nate.com>
2022-05-27 00:13:21 +0900

    🦊 key bind: sprint

--- Source/SuperSideScroller/SuperSideScroller_Player.cpp
+++ Source/SuperSideScroller/SuperSideScroller_Player.cpp
@@ -2,4 +2,43 @@
 
 
 #include "SuperSideScroller_Player.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+ASuperSideScroller_Player::ASuperSideScroller_Player()
+{
+	// bIsSprinting 값을 기본적으로 false로 설정
+	bIsSprinting = false;
+
+	// MaxWalkSpeed 값을 300.0f로 설정
+	GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+}
+
+void ASuperSideScroller_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// sprint 입력이 눌렸을 때 Sprint 함수를 호출하도록 바인딩 (연결) 하기
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &ASuperSideScroller_Player::Sprint);
+
+	// sprint 입력이 해제됐을 때 StopSprinting 함수를 호출하도록 바인딩 (연결) 하기
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &ASuperSideScroller_Player::StopSprinting);
+}
+
+void ASuperSideScroller_Player::Sprint()
+{
+	if (!bIsSprinting)
+	{
+		bIsSprinting = true;
+		GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	}
+}
+
+void ASuperSideScroller_Player::StopSprinting()
+{
+	if (bIsSprinting)
+	{
+		bIsSprinting = false;
+		GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+	}
+}
--- Source/SuperSideScroller/SuperSideScroller_Player.h
+++ Source/SuperSideScroller/SuperSideScroller_Player.h
@@ -13,5 +13,21 @@ UCLASS()
 class SUPERSIDESCROLLER_API ASuperSideScroller_Player : public ASuperSideScrollerCharacter
 {
 	GENERATED_BODY()
+
+public:
+	ASuperSideScroller_Player();
+
+protected:
+	// 플레이어 입력 컴포넌트를 설정하기 위해 부모 캐릭터 클래스의 함수를 재정의한다.
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	// 전력 질주
+	void Sprint();
+
+	// 전력 질주 정지
+	void StopSprinting();
 	
+private:
+	// 전력 질주 중인지 확인하는 bool 변수. 안전장치.
+	bool bIsSprinting;
 };
